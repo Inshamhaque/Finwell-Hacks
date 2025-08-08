2568f085dfc005f0df19508561429627269f65e1
Kuldeep kumar <117709642+Kuldeep12mohan@users.noreply.github.com>
2025-08-08 16:23:32 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,3 @@
 const Production = import.meta.PRODUCTION
-// export const BACKEND_URL=Production?'https://finwell-server.onrender.com':"http://localhost:3000"
-export const BACKEND_URL = "http://localhost:3000"
+export const BACKEND_URL=Production?'https://finwell-server.onrender.com':"http://localhost:3000"
 console.log(BACKEND_URL)
